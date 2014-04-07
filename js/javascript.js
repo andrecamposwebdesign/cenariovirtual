ae7e876939b9e18e41964b95a44aa15d5817f698
unknown <andrecampos.webdesign@gmail.com>
2014-04-07 09:35:58 -0300

    tudo

---
+++ js/javascript.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  	$( ".banner, .especialies" ).fadeIn( 2500 );
+});
+
+
+$( "a" ).click(function() {
+  	$( ".banner" ).hide( 1000 );
+});
+
+
+
+$(document).ready(function() {
+  	$( ".ad1" ).delay( 9000 ).fadeOut( 900 );
+});
+
+$(document).ready(function() {
+  	$( ".ad2" ).delay( 9920 ).fadeIn( 900 );
+  	$( ".ad2" ).delay( 9000 ).fadeOut( 900 );
+});
+
+$(document).ready(function() {
+  	$( ".ad3" ).delay( 20740 ).fadeIn( 900 );
+});
+
+
+
+$(window).load(function() {
+	$('.loading').fadeOut();
+});
